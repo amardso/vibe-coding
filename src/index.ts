349c0fab58a1a78dd5328b90de76821295f02184
A Samsul Amar <amar.bahasakita@gmail.com>
2026-05-07 11:32:22 +0700

    feat: implement unit tests for user APIs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import { Elysia } from 'elysia';
-import { usersRoute } from './routes/users-route';
+import { app } from './app';
 
-const app = new Elysia()
-  .get('/', () => 'Hello Elysia!')
-  .use(usersRoute)
-  .listen(3000);
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
